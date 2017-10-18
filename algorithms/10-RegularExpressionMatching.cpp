7f30244726bd43192c520d5030406681443145e0
qmarliu <liu6uu@163.com>
2017-10-18 16:21:52 +0800

    添加10-RegularExpressionMatching.cpp自己的解答

---
+++ algorithms/10-RegularExpressionMatching.cpp
@@ -0,0 +1,197 @@
+/*
+ *Description:
+ ******************
+ *Implement regular expression matching with support for '.' and '*'
+    '.' Matches any single character.
+    '*' Matches zero or more of the preceding element.
+
+    The matching should cover the entire input string (not partial).
+
+    The function prototype should be:
+    bool isMatch(const char *s, const char *p)
+
+    Some examples:
+    isMatch("aa","a") → false
+    isMatch("aa","aa") → true
+    isMatch("aaa","aa") → false
+    isMatch("aa", "a*") → true
+    isMatch("aa", ".*") → true
+    isMatch("ab", ".*") → true
+    isMatch("aab", "c*a*b") → true
+ *******************
+ */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    //自己实现的，Runtime: 6ms
+    bool isMatch(string s, string p)
+    {
+        if (p.empty())
+            return s.empty();
+        if (p.size() == 1)
+            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
+
+        int i = 0, j = 0;
+        vector<int> pointJ;
+        vector<int> pointI;
+        while (i < s.size() && j < p.size())
+        {
+            if (j + 1 >= p.size())
+            {
+                if (i + 1 >= s.size())
+                    return s[i] == p[j] || p[j] == '.';
+                else
+                {
+                    if (pointJ.size() != 0)
+                    {
+                        i = ++pointI.back();
+                        j = pointJ.back() + 2;
+                        if (p[j - 2] != '.')
+                        {
+                            if (p[j - 2] != s[i])
+                            {
+                                pointJ.pop_back();
+                                pointI.pop_back();
+                            }
+                        }
+                        continue;
+                    }
+                    return false;
+                }
+            }
+            if (p[j + 1] == '*')
+            {
+                if (s[i] == p[j] || p[j] == '.')
+                {
+                    if (j + 2 >= p.size())
+                    {
+                        if (p[j] == '.')
+                            return true;
+                        else
+                        {
+                            ++i;
+                            continue;
+                        }
+                    }
+                    pointJ.push_back(j);
+                    pointI.push_back(i);
+                    ++j, ++j;
+                    continue;
+                }
+                ++j, ++j;
+                if (j >= p.size())
+                {
+                    if (pointJ.size() != 0)
+                    {
+                        i = ++pointI.back();
+                        j = pointJ.back() + 2;
+                        if (p[j - 2] != '.')
+                        {
+                            if (p[j-2] != s[i])
+                            {
+                                pointI.pop_back();
+                                pointJ.pop_back();
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (s[i] == p[j] || p[j] == '.')
+                {
+                    ++i; ++j;
+                    continue;
+                }
+                else if (pointJ.size() != 0)
+                {
+                    i = ++pointI.back();
+                    j = pointJ.back() + 2;
+                    if (p[j - 2] != '.')
+                    {
+                        if (p[j-2] != s[i])
+                        {
+                            pointI.pop_back();
+                            pointJ.pop_back();
+                        }
+                    }
+                    continue;
+                }
+                return false;
+            }
+        }
+        if (i >= s.size())
+        {
+            if (j >= p.size())
+                return true;
+            if (j + 1 == p.size())
+            {
+                return p[j+1] == '*';
+            }
+            if (p[j + 1] == '*')
+            {
+                ++j, ++j;
+                while (j < p.size())
+                {
+                    if (j + 1 < p.size() && p[j + 1] == '*')
+                    {
+                        ++j, ++j;
+                        continue;
+                    }
+                    return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //leetCode上最好的回答 Runtime: 3ms
+    bool isMatch2(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] != '*')
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.'); 
+                else
+                    dp[i][j] = dp[i][j - 2] || i > 0 && (s[i - 1] == p[j - 2] || p[j - 2] == '.') && dp[i - 1][j];
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+int main( int argc, char **argv )
+{
+    Solution s;
+    cout << s.isMatch("ab", ".*a*a*") << s.isMatch2("ab", ".*a*a*") <<endl;
+    cout << s.isMatch("abb", ".*b*") << s.isMatch2("abb", ".*b*") << endl;
+    cout << s.isMatch("ab", ".*b*") << s.isMatch2("ab", ".*b*") << endl;
+    cout << s.isMatch("aaba", "ab*a*c*a") << s.isMatch2("aaba", "ab*a*c*a") << endl;
+    cout << s.isMatch("abcd", "ab*bcd") << s.isMatch2("abcd", "ab*bcd") << endl;
+    cout << s.isMatch("a", "ab*a") << s.isMatch2("a", "ab*a") << endl;
+    cout << s.isMatch("aaa", "ab*a*c*a") << s.isMatch2("aaa", "ab*a*c*a") << endl;
+    cout << s.isMatch("ab", ".*") << s.isMatch2("ab", ".*") << endl;
+    cout << s.isMatch("aa", ".*") << s.isMatch2("aa", ".*") << endl;
+    cout << s.isMatch("aa", "aa") << s.isMatch2("aa", "aa") << endl;
+    cout << s.isMatch("acc", "abc") << s.isMatch2("acc", "abc") << endl;
+    cout << s.isMatch("abc", "ab*") << s.isMatch2("abc", "ab*") << endl;
+    cout << s.isMatch("abcdabcdad", "a*da*d") << s.isMatch2("abcdabcdad", "a*da*d") << endl;
+    cout << s.isMatch("aab", "c*a*b") << s.isMatch2("aab", "c*a*b") << endl;
+    cout << s.isMatch("abcd", "d*") << s.isMatch2("abcd", "d*") << endl;
+    cout << s.isMatch("aa", "a") << s.isMatch2("aa", "a") << endl;
+    cout << s.isMatch("ab", ".*c") << s.isMatch2("ab", ".*c") << endl;
+    cout << s.isMatch("abcccccc", "a.*c") << s.isMatch2("abcccccc", "a.*c") << endl;
+    getchar();
+    return 0;
+}
---
+++ algorithms/9-PalindromeNumber.cpp
@@ -0,0 +1,66 @@
+/*
+ *Description:
+ ******************
+ *Determine whether an integer is a palindrome. Do this without extra space.
+
+ *******************
+ */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    //自己实现的，Runtime: 148ms
+    bool isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        //取出x中每位的值，并存储到queue中
+        deque<int> iq;
+        while (x != 0)
+        {
+            int min = x % 10;
+            x = x / 10;
+            iq.push_back(min);
+       }
+       while(iq.size() > 1)
+       {
+           if (iq.front() != iq.back())
+               return false;
+            iq.pop_front();
+            iq.pop_back();
+       }
+       return true;
+    }
+
+    //leetcode上最好的回答： 62ms
+    bool isPalindrome2(int x) {
+        if(x<0||(x%10==0&&x!=0))
+            return false;
+        if(x<10)
+            return true;
+        int n = x;
+        int rem, sum=0;
+        while(n>sum){
+            rem = n%10;
+            sum = sum*10+rem;
+            n /= 10;
+        }
+        return n==sum || n==(sum/10);
+    }
+};
+
+int main( int argc, char **argv )
+{
+    Solution s;
+    cout << (s.isPalindrome(-2147447412) ? " is Palindrome." : " not Palindrome.") << endl;
+    cout << (s.isPalindrome(123) ? " is Palindrome." : " not Palindrome.") << endl;
+    cout << (s.isPalindrome(121) ? " is Palindrome." : " not Palindrome.") << endl;
+    getchar();
+    return 0;
+}
